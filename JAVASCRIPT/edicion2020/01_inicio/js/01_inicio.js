46e64e093fe250b520c30ded44ae55251670a46a
marco <marco_broter@hotmail.com>
2021-01-24 15:26:04 -0500

    cadenas-string

--- JAVASCRIPT/edicion2020/01_inicio/js/01_inicio.js
+++ JAVASCRIPT/edicion2020/01_inicio/js/01_inicio.js
@@ -1,35 +1,28 @@
 const c=console.log;
-/* let a = 5;
-let b = 2;
-let usuario="Carlo";
-c(a+b);
-c(a-b);
-c(a*b);
-c(a/b);
-c(a%b);
-
-c("hola "+usuario); */
-//operadores de incremento y decremento
-
-// let a=5;
-// c(a)
-// c(a++)
-// c(a)
-// c(++a)
-// c(a)
-// c(--a)
-
-let a=10;
-// a=a+3;
-a+=3;
-c(a)
-a-=3;
-c(a)
-a*=3;
-c(a)
-a/=3;
-c(a)
-a%=3;
-c(a)
+let nombre="Juan";
+let apellido="Gomez";
+let edad=20;
 
 
+c(`Hola mi nombre es ${nombre} ${apellido} y tengo ${edad} `);
+// c("Hola "+nombre+edad+apellido+" tiene ")
+
+
+// let cadena="        Hola Mundo";
+// c(cadena.indexOf("Mundo"))
+// c(cadena.substring(3))
+
+// c(cadena.endsWith("o"))
+// c(cadena.includes("o"))
+// let cadena3="Hola ";
+// c(cadena3.repeat(3))
+
+
+
+
+
+
+/* 
+c(cadena)
+c(cadena.toLowerCase())
+c(cadena.toUpperCase()) */
