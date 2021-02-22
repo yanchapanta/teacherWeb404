f660771772621d122e5a2d611e3dd5d21e0a3338
marco <marco_broter@hotmail.com>
2021-02-22 17:37:47 -0500

    gitflow-feature-stash: ejercicio37start

---
+++ JAVASCRIPT/03 EstructuraSelectivaMultiple/problema_37/js/problema_37.js
@@ -0,0 +1,11 @@
+'use strict'
+const c=console.log;
+import showhDataaa from './ddbb.js'
+const newPost=[
+    {data1:6,data2:6,data3:6},
+    {data1:5,data2:6,data3:6},
+    {data1:4,data2:5,data3:3},
+    {data1:3,data2:6,data3:2}
+    
+]
+showhDataaa(newPost);
